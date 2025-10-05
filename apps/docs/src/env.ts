849d5f0c72c167886679749219fc418a0d7312a9
Adophilus <uchenna19of@gmail.com>
2025-10-05 14:07:53 +0100

    chore: updated overview

--- apps/docs/src/env.ts
+++ apps/docs/src/env.ts
@@ -1,12 +1,11 @@
-import { createEnv } from '@t3-oss/env-core'
-import { z } from 'zod'
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
 
 export const env = createEnv({
-  clientPrefix: 'VITE_',
-  client: {
-    VITE_NODE_ENV: z.enum(['production', 'staging', 'development', 'test']),
-    VITE_API_URL: z.string().url()
-  },
-  server: {},
-  runtimeEnv: import.meta.env
-})
+	clientPrefix: "VITE_",
+	client: {
+		VITE_NODE_ENV: z.enum(["production", "staging", "development", "test"]),
+	},
+	server: {},
+	runtimeEnv: import.meta.env,
+});
