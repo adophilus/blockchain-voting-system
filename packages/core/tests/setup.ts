dc8b97da51346d9008f0f85f63309208dcdde4d9
Adophilus <uchenna19of@gmail.com>
2025-10-07 01:15:00 +0100

    chore: began writign the test cases for the system

--- packages/core/tests/setup.ts
+++ packages/core/tests/setup.ts
@@ -1,26 +1,96 @@
 import { createPublicClient, createWalletClient, http, type Hex } from "viem";
 import { foundry } from "viem/chains";
-import { env } from "../src/env";
+import { env } from "./env";
 import { Wallet } from "../src/wallet";
 import { privateKeyToAccount } from "viem/accounts";
 
 const chain = foundry;
 
-const privateKey = env.PRIVATE_KEY as Hex;
+let deployerWallet: Wallet;
+let candidate1Wallet: Wallet;
+let candidate2Wallet: Wallet;
+let voter1Wallet: Wallet;
+let voter2Wallet: Wallet;
 
-const account = privateKeyToAccount(privateKey) as any;
+{
+	const privateKey = env.PRIVATE_KEY as Hex;
+	const account = privateKeyToAccount(privateKey) as any;
+	const publicClient = createPublicClient({
+		chain,
+		transport: http(),
+	});
+	const walletClient = createWalletClient({
+		chain,
+		transport: http(),
+		account,
+	});
+	deployerWallet = new Wallet(privateKey, publicClient, walletClient);
+}
 
-const publicClient = createPublicClient({
-	chain,
-	transport: http(),
-});
+{
+	const privateKey = env.CANDIDATE_1_PRIVATE_KEY as Hex;
+	const account = privateKeyToAccount(privateKey) as any;
+	const publicClient = createPublicClient({
+		chain,
+		transport: http(),
+	});
+	const walletClient = createWalletClient({
+		chain,
+		transport: http(),
+		account,
+	});
+	candidate1Wallet = new Wallet(privateKey, publicClient, walletClient);
+}
 
-const walletClient = createWalletClient({
-	chain,
-	transport: http(),
-	account,
-});
+{
+	const privateKey = env.CANDIDATE_2_PRIVATE_KEY as Hex;
+	const account = privateKeyToAccount(privateKey) as any;
+	const publicClient = createPublicClient({
+		chain,
+		transport: http(),
+	});
+	const walletClient = createWalletClient({
+		chain,
+		transport: http(),
+		account,
+	});
+	candidate2Wallet = new Wallet(privateKey, publicClient, walletClient);
+}
 
-const wallet = new Wallet(privateKey, publicClient, walletClient);
+{
+	const privateKey = env.VOTER_1_PRIVATE_KEY as Hex;
+	const account = privateKeyToAccount(privateKey) as any;
+	const publicClient = createPublicClient({
+		chain,
+		transport: http(),
+	});
+	const walletClient = createWalletClient({
+		chain,
+		transport: http(),
+		account,
+	});
+	voter1Wallet = new Wallet(privateKey, publicClient, walletClient);
+}
 
-export { wallet };
+{
+	const privateKey = env.VOTER_2_PRIVATE_KEY as Hex;
+	const account = privateKeyToAccount(privateKey) as any;
+	const publicClient = createPublicClient({
+		chain,
+		transport: http(),
+	});
+	const walletClient = createWalletClient({
+		chain,
+		transport: http(),
+		account,
+	});
+	voter2Wallet = new Wallet(privateKey, publicClient, walletClient);
+}
+
+export {
+	deployerWallet,
+	candidate1Wallet,
+	candidate2Wallet,
+	voter1Wallet,
+	voter2Wallet,
+};
