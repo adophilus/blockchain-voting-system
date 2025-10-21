d70714bce28a4e41e049d1694130b8c78872cc0f
Adophilus <uchenna19of@gmail.com>
2025-10-21 06:29:32 +0100

    chore: updated ci

--- apps/backend/scripts/create-admin.ts
+++ apps/backend/scripts/create-admin.ts
@@ -1,45 +1,45 @@
-import { AuthUserRepository } from '@/features/auth/repository'
-import { hashPassword } from '@/features/auth/utils/password'
-import { Container } from '@n8n/di'
-import { bootstrap } from '@blockchain-voting-system/backend'
-import { ulid } from 'ulidx'
+import { AuthUserRepository } from "@/features/auth/repository";
+import { hashPassword } from "@/features/auth/utils/password";
+import { Container } from "@n8n/di";
+import { bootstrap } from "@blockchain-voting-system/backend";
+import { ulid } from "ulidx";
 
-await bootstrap()
+await bootstrap();
 
 const adminPayload = {
-  id: ulid(),
-  email: 'topnotch@futo.edu.ng',
-  password_hash: await hashPassword('{%gJ$mbxT+07V:Zi'),
-  full_name: 'Admin',
-  role: 'ADMIN'
-} as const
+	id: ulid(),
+	email: "admin@university.edu",
+	password_hash: await hashPassword("admin123"),
+	full_name: "Admin",
+	role: "ADMIN",
+} as const;
 
-const authUserRepository = Container.get(AuthUserRepository)
+const authUserRepository = Container.get(AuthUserRepository);
 
 const createAdmin = async (authUserRepository: AuthUserRepository) => {
-  const existingUserResult = await authUserRepository.findByEmail(
-    adminPayload.email
-  )
-  if (existingUserResult.isErr) {
-    console.log('❌ Failed to check existing user')
-    return
-  }
+	const existingUserResult = await authUserRepository.findByEmail(
+		adminPayload.email,
+	);
+	if (existingUserResult.isErr) {
+		console.log("❌ Failed to check existing user");
+		return;
+	}
 
-  const existingUser = existingUserResult.value
-  if (existingUser) {
-    console.log('❌ Admin user already exists')
-    return
-  }
+	const existingUser = existingUserResult.value;
+	if (existingUser) {
+		console.log("❌ Admin user already exists");
+		return;
+	}
 
-  const res = await authUserRepository.create(adminPayload)
+	const res = await authUserRepository.create(adminPayload);
 
-  if (res.isErr) {
-    console.log(res.error)
-    console.log('❌ Failed to create admin user')
-    return
-  }
+	if (res.isErr) {
+		console.log(res.error);
+		console.log("❌ Failed to create admin user");
+		return;
+	}
 
-  console.log('✅ Admin user created')
-}
+	console.log("✅ Admin user created");
+};
 
-await createAdmin(authUserRepository)
+await createAdmin(authUserRepository);
